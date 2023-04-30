ac7b3ed75e94156d3ca218e6431c859019be996b
Anonymous <anonymous@example.com>
2023-04-30 17:39:56 +0300

    fix rtl, form text and question ids

--- render_mos.py
+++ render_mos.py
@@ -2,7 +2,7 @@
 """Generate forms for human evaluation."""
 
 from jinja2 import FileSystemLoader, Environment
-
+from pathlib import Path
 
 def main():
     """Main function."""
@@ -10,22 +10,21 @@ def main():
     env = Environment(loader=loader)
     template = env.get_template("mos.html.jinja2")
 
+    wavs = sorted(Path('samples_mos').glob('*wav'))
+    questions = [
+        {
+            "title" : f"שמע {i+1}",
+            "audio_path" : str(p),
+            "name" : f"q_{i+1}_{p.name.replace(' ','-')}"
+        } for i, p in enumerate(wavs)
+    ]
+
+
     html = template.render(
-        page_title="MOS 實驗表單 1",
-        form_url="http://localhost:8888",
+        page_title="MOS רובו-שאול",
+        form_url="https://script.google.com/macros/s/AKfycbwts5ghKot09txpxiO_rbbv2qyaO5U_U6N-kEQL7XGvY-RgkPYvQgekmud4-CQ8aKDRww/exec",
         form_id=1,
-        questions=[
-            {
-                "title": "問題 1",
-                "audio_path": "wavs/test1.wav",
-                "name": "q1"
-            },
-            {
-                "title": "問題 2",
-                "audio_path": "wavs/test2.wav",
-                "name": "q2"
-            },
-        ]
+        questions=questions,
     )
     print(html)
 
